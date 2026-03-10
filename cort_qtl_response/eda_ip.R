66dbdc001e9ae1802100e1bc938da483b2ce4d2d
rnarendr <orukja@gmail.com>
2026-03-10 10:13:43 -0700

    added eda script to tracking

---
+++ cort_qtl_response/eda_ip.R
@@ -0,0 +1,53 @@
+args <- commandArgs(trailingOnly = TRUE)
+if(length(args) != 1)  stop("Incorrect number of arguments")
+
+opt_input  <- args[1]
+temp <- read.table(opt_input,nrows = 1)
+
+header <- FALSE
+if (temp$V1 == "phe_id" || temp$V1 == "grp_id"){
+  header  <- TRUE
+  bwd_pval <- which(temp == "bwd_pval")
+  if(length(bwd_pval) != 1)  stop("Header present but cannot find column bwd_pval")
+  bwd_r_squared <- which(temp == "bwd_r_squared")
+  if(length(bwd_r_squared) != 1)  stop("Header present but cannot find column bwd_r_squared")
+  bwd_slope <- which(temp == "bwd_slope")
+  if(length(bwd_slope) != 1)  stop("Header present but cannot find column bwd_slope")
+  bwd_best_hit <- which(temp == "bwd_best_hit")
+  if(length(bwd_best_hit) != 1)  stop("Header present but cannot find column bwd_best_hit")
+  rank <- which(temp == "rank")
+  if(length(rank) != 1)  stop("Header present but cannot find column rank")
+}
+D = read.table(opt_input,hea=header, stringsAsFactors=FALSE)
+
+## Create order columns
+
+D_ranks <- D %>%
+  group_by(phe_id) %>%
+  mutate(
+    magnitude_bwd_slope = abs(bwd_slope),
+    
+    slope_rank = dense_rank(-magnitude_bwd_slope),
+    rsq_rank   = dense_rank(-bwd_r_squared),
+    adj_pval_rank = dense_rank(bwd_pval),
+    
+    score = magnitude_bwd_slope * -log10(pmax(bwd_pval, 1e-300)),
+    rsq_score = magnitude_bwd_slope * -log10(pmax(bwd_pval, 1e-300)) * bwd_r_squared,
+    
+    score_rank = dense_rank(-score),
+    rsq_score_rank = dense_rank(-rsq_score)
+  )
+  
+
+ggplot(D_ranks, aes(x = as.factor(bwd_best_hit), y = score_rank)) +
+  geom_boxplot()
+
+ggplot(D_ranks, aes(x = as.factor(bwd_best_hit), y = slope_rank)) +
+  geom_boxplot()
+
+ggplot(D_ranks, aes(x = as.factor(bwd_best_hit), y = score)) +
+  geom_boxplot()
+
+
+
+
